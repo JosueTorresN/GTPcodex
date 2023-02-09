63cf1b925410f176f78ad7ffe58c89207595e3be
Josue TorresN <jtn1999@estudiantec.cr>
2023-02-08 18:17:31 -0600

    update server url

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmint = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://gtp3asist.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
